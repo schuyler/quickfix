ea77afbc2106f33d92cc71d29451cf157f4b794f
Schuyler Erle <schuyler@nocat.net>
2018-03-26 01:09:12 -0700

    Split out testing options into a YAML config.

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math, time
+import math, time, yaml
 from quickfix import Beacon2D
 
 def generate_anchor_positions(n, bounds, dim=3):
@@ -11,7 +11,7 @@ def generate_tag_position(bounds, dim=3):
 def compute_actual_ranges(actual, anchors):
     return np.linalg.norm(anchors - actual, axis=1)
 
-def generate_noise(shape, factor=0.25):
+def generate_sensor_noise(shape, factor=0.25):
     # Assume measurement noise is zero-centered Gaussian
     return np.random.normal(0.0, factor, shape)
 
@@ -19,7 +19,7 @@ def generate_nlos_noise(shape, factor=1.):
     # Assume NLOS noise is always positive and multiplicative
     # on a logistic distribution
     return np.clip( 
-        np.random.logistic(-1., 1., shape),
+        np.random.logistic(-1., factor, shape),
         1.,
         None
     )
@@ -27,51 +27,51 @@ def generate_nlos_noise(shape, factor=1.):
 def distance(a, b):
     return np.linalg.norm(a - b)
 
-def run_test(tag, actual_position, actual_ranges, bounds, noise_std=0.15, filtered=True, debug=False):
+def run_test(tag, actual_position, actual_ranges, bounds,
+        sensor_noise=1., nlos_noise=1., mse_target=18., debug=False):
     def d(*args):
         if debug: print " ".join(map(repr, args))
     shape = actual_ranges.shape
 
-    noise = generate_noise(shape, noise_std)
-    d("     noise:", noise)
-    nlos_noise = generate_nlos_noise(shape)
-    d("nlos noise:", nlos_noise)
-    noisy_ranges = (actual_ranges + noise) * nlos_noise
-    #noisy_ranges = actual_ranges + noise
+    s_noise = generate_sensor_noise(shape, sensor_noise)
+    d("     noise:", s_noise)
+    n_noise = generate_nlos_noise(shape, nlos_noise)
+    d("nlos noise:", n_noise)
+    noisy_ranges = (actual_ranges + s_noise) * n_noise
 
     for i, r in enumerate(noisy_ranges):
         tag.range(i, r)
 
-    guess, mse = tag.fix(18.)
+    guess, mse = tag.fix(mse_target)
+    """
     if guess[0] == bounds[0][0] or \
        guess[1] == bounds[0][1] or \
        guess[0] == bounds[1][1] or \
        guess[1] == bounds[1][1]:
            pass
-           #print "stop:", guess
-           #raise
+    """
 
     error = distance(guess, actual_position)
     d("act:", actual_position, "upd:", guess, "err:", round(error, 6))
     return error, np.sqrt(mse)
 
-def run_tests(m=10000, n_anchors=7, noise=0.05, debug=False):
+def run_tests(runs=10000, anchors=7, sensor_noise=6., nlos_noise=1.,
+    bounds=[[-1000., -500.], [1000., 500.]], rmse=18., debug=False):
     dim = 2
-    bounds = [[-1000., -500.], [1000., 500.]]
-    noise = 6.
     errors = []
     rms_errors = []
 
-    for _ in range(m):
-        anchors = generate_anchor_positions(n_anchors, bounds, dim)
+    for _ in range(runs):
+        anchor_set = generate_anchor_positions(anchors, bounds, dim)
         actual_position = generate_tag_position(bounds, dim)
-        actual_ranges = compute_actual_ranges(actual_position, anchors)
+        actual_ranges = compute_actual_ranges(actual_position, anchor_set)
 
         try:
             tag = Beacon2D(bounds)
-            for i, a in enumerate(anchors):
+            for i, a in enumerate(anchor_set):
                 tag.anchor(i, a)
-            err, rms_err = run_test(tag, actual_position, actual_ranges, bounds, noise, debug)
+            err, rms_err = run_test(tag, actual_position, actual_ranges, bounds,
+                                    sensor_noise, nlos_noise, rmse, debug)
             errors.append(err)
             rms_errors.append(rms_err)
         except Exception, e:
@@ -82,8 +82,9 @@ def run_tests(m=10000, n_anchors=7, noise=0.05, debug=False):
     return len(errors), errors.mean(), errors.std(), rms_errors.mean(), rms_errors.std()
 
 if __name__ == "__main__":
+    cfg = yaml.load(open("test.yaml", "r"))
     start = time.time()
-    n, mean, std, rms_mean, rms_std = run_tests()
+    n, mean, std, rms_mean, rms_std = run_tests(**cfg)
     elapsed = time.time() - start
     print "tests run:", n
     print "mean err:", mean
